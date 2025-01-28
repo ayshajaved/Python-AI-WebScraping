e52ee401e1c781bc2d40cbd9730fbed9313bc6fa
Ayesha Javed <ayshajavd@gmail.com>
2025-01-28 10:07:27 +0500

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("Python AI WebScraping")
-url = st.text_input("Enter URL of a Website to Scrap: ")
-if st.button("Scrape"):
-    st.write("Scraping Website")
-    
